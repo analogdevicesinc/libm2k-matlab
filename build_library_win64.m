d1295e6fa2c05e7b7c3d5a9312b4bcd485eb28e0
Travis Collins <travis.collins@analog.com>
2020-07-15 11:05:45 -0700

    Add missing methods for windows
    Signed-off-by: Travis Collins <travis.collins@analog.com>

--- build_library_win64.m
+++ build_library_win64.m
@@ -21,26 +21,48 @@ end
 %% Add related headers
 h = {};
 
-% h1 = fullfile(pwd,'libm2k','include','iio.h');  h = [{h1},h(:)'];
-h1 = fullfile(hppPath,'m2kcalibration.hpp'); h = [{h1},h(:)'];
-h1 = fullfile(hppPath,'m2kexceptions.hpp'); h = [{h1},h(:)'];
-h1 = fullfile(hppPath,'logger.hpp'); h = [{h1},h(:)'];
-% h1 = fullfile(hppPath,'digital','genericdigital.hpp'); h = [{h1},h(:)'];
+% h1 = fullfile(hppPath,'m2kcalibration.hpp'); h = [{h1},h(:)'];
+% h1 = fullfile(hppPath,'m2kexceptions.hpp'); h = [{h1},h(:)'];
+% h1 = fullfile(hppPath,'logger.hpp'); h = [{h1},h(:)'];
+% h1 = fullfile(hppPath,'digital','m2kdigital.hpp'); h = [{h1},h(:)'];
+% h1 = fullfile(hppPath,'analog','m2kanalogout.hpp'); h = [{h1},h(:)'];
+% h1 = fullfile(hppPath,'analog','m2kanalogin.hpp'); h = [{h1},h(:)'];
+% h1 = fullfile(hppPath,'m2kcalibration.hpp'); h = [{h1},h(:)'];
+% h1 = fullfile(hppPath,'m2khardwaretrigger.hpp'); h = [{h1},h(:)'];
+% h1 = fullfile(hppPath,'analog','m2kpowersupply.hpp'); h = [{h1},h(:)'];
+% h1 = fullfile(hppPath,'utils','utils.hpp'); h = [{h1},h(:)'];
+% h1 = fullfile(hppPath,'utils','enums.hpp'); h = [{h1},h(:)'];
+% h1 = fullfile(hppPath,'contextbuilder.hpp'); h = [{h1},h(:)'];
+% h1 = fullfile(hppPath,'m2k.hpp'); h = [{h1},h(:)'];
+% h1 = fullfile(hppPath,'m2kglobal.hpp'); h = [{h1},h(:)'];
+% h1 = fullfile(hppPath,'context.hpp'); h = [{h1},h(:)'];
+% h1 = fullfile(hppPath,'digital','enums.hpp'); h = [{h1},h(:)'];
+% h1 = fullfile(hppPath,'analog','enums.hpp'); h = [{h1},h(:)'];
+% h1 = fullfile(hppPath,'enums.hpp'); h = [{h1},h(:)'];
+% % h1 = fullfile(pwd,'libm2k','include','iio.h');  h = [{h1},h(:)'];
+% headers = h;
+
 h1 = fullfile(hppPath,'digital','m2kdigital.hpp'); h = [{h1},h(:)'];
+h1 = fullfile(hppPath,'digital','enums.hpp'); h = [{h1},h(:)'];
+
 h1 = fullfile(hppPath,'analog','m2kanalogout.hpp'); h = [{h1},h(:)'];
 h1 = fullfile(hppPath,'analog','m2kanalogin.hpp'); h = [{h1},h(:)'];
+h1 = fullfile(hppPath,'analog','m2kpowersupply.hpp'); h = [{h1},h(:)'];
+h1 = fullfile(hppPath,'analog','dmm.hpp'); h = [{h1},h(:)'];
+h1 = fullfile(hppPath,'analog','enums.hpp'); h = [{h1},h(:)'];
+
+% h1 = fullfile(hppPath,'utils','utils.hpp'); h = [{h1},h(:)'];
+% h1 = fullfile(hppPath,'utils','enums.hpp'); h = [{h1},h(:)'];
+
 h1 = fullfile(hppPath,'m2kcalibration.hpp'); h = [{h1},h(:)'];
 h1 = fullfile(hppPath,'m2khardwaretrigger.hpp'); h = [{h1},h(:)'];
-h1 = fullfile(hppPath,'analog','m2kpowersupply.hpp'); h = [{h1},h(:)'];
-% h1 = fullfile(hppPath,'analog','powersupply.hpp'); h = [{h1},h(:)'];
-h1 = fullfile(hppPath,'utils','utils.hpp'); h = [{h1},h(:)'];
-h1 = fullfile(hppPath,'utils','enums.hpp'); h = [{h1},h(:)'];
 h1 = fullfile(hppPath,'contextbuilder.hpp'); h = [{h1},h(:)'];
 h1 = fullfile(hppPath,'m2k.hpp'); h = [{h1},h(:)'];
 h1 = fullfile(hppPath,'m2kglobal.hpp'); h = [{h1},h(:)'];
 h1 = fullfile(hppPath,'context.hpp'); h = [{h1},h(:)'];
-h1 = fullfile(hppPath,'digital','enums.hpp'); h = [{h1},h(:)'];
-h1 = fullfile(hppPath,'analog','enums.hpp'); h = [{h1},h(:)'];
+h1 = fullfile(hppPath,'logger.hpp'); h = [{h1},h(:)'];
+h1 = fullfile(hppPath,'m2kcalibration.hpp'); h = [{h1},h(:)'];
+h1 = fullfile(hppPath,'m2kexceptions.hpp'); h = [{h1},h(:)'];
 h1 = fullfile(hppPath,'enums.hpp'); h = [{h1},h(:)'];
 % h1 = fullfile(pwd,'libm2k','include','iio.h');  h = [{h1},h(:)'];
 headers = h;
